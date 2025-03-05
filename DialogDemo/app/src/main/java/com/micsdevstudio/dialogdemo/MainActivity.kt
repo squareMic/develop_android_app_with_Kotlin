2d9cbb27286c231a37fbb277f4501a857bd81220
Mic <michaelmay179@gmail.com>
2025-03-05 23:43:45 +0100

    Chapter Dialog windows

---
+++ DialogDemo/app/src/main/java/com/micsdevstudio/dialogdemo/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.micsdevstudio.dialogdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        // We could have removed the previous line of code by
+        // adding the ...synthetic.main.activity_main.* import
+        // as an alternative
+
+        button.setOnClickListener {
+            val myDialog = MyDialog()
+            myDialog.show(supportFragmentManager, "123")
+            // This calls onCreateDialog
+            // Don't worry about the strange looking 123
+            // We will find out about this in chapter 18
+        }
+    }
+}
+
