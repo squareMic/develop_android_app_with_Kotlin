f61a905aaf223b0f2249ff869d44a31b83853857
Mic <michaelmay179@gmail.com>
2025-03-11 17:13:11 +0100

    Add new file and class Note

---
+++ NotetoSelf/app/src/main/java/com/micsdevstudio/notetoself/Note.kt
@@ -0,0 +1,10 @@
+package com.micsdevstudio.notetoself
+
+class Note {
+    var title: String? = null
+    var description: String? = null
+    var idea: Boolean = false
+    var todo: Boolean = false
+    var important: Boolean = false
+    
+}
